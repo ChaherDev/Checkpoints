a752721b4c1324c40af700314ac856b5b36a6542
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-08-27 16:58:41 +0200

    Checkpoint 1 completed

--- Checkpoint 1.playground/Contents.swift
+++ Checkpoint 1.playground/Contents.swift
@@ -1,3 +1,7 @@
 import Cocoa
 
+let temperatureInCelsius = 532
 
+let temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32
+
+print("The temperature \(temperatureInCelsius)°C corresponds to that in \(temperatureInFahrenheit)°F")
