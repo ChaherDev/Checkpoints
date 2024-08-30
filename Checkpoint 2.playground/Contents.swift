5e7bebfa05a866cf455c353dbf0638d0d7b39523
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-08-30 11:05:35 +0200

    Checkpoint 2 completed

--- Checkpoint 2.playground/Contents.swift
+++ Checkpoint 2.playground/Contents.swift
@@ -1,2 +1,9 @@
 import Cocoa
 
+var peoples = ["LeBron James", "Kylian Mbappé", "Emmanuel Macron", "Kylian Mbappé"]
+
+print(peoples.count)
+
+var peoplesWithoutDuplicate = Set(peoples)
+
+print(peoplesWithoutDuplicate)
