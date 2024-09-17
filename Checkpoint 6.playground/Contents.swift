a37f88929848c619f85621f7a140c602b6b6e5ea
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-09-17 16:26:49 +0200

    Checkpoint 6 completed

--- Checkpoint 6.playground/Contents.swift
+++ Checkpoint 6.playground/Contents.swift
@@ -1,2 +1,24 @@
 import Cocoa
 
+struct Car {
+    let model: String
+    let numberOfSeats: Int
+    private(set) var currentGear: Int
+    
+    mutating func changeGear(to newGear: Int) {
+        if newGear >= 0 && newGear <= 6 {
+            currentGear = newGear
+        } else {
+            print("Gear \(newGear) is out of range. Please choose a gear between 0 and 6.")
+        }
+    }
+}
+
+var myCar = Car(model: "Toyota Corolla", numberOfSeats: 5, currentGear: 1)
+print("Current gear: \(myCar.currentGear)")
+
+myCar.changeGear(to: 3)
+print("Gear after changing: \(myCar.currentGear)")
+
+myCar.changeGear(to: 7)  // Rapport de vitesse invalide
+print("Gear after invalid change: \(myCar.currentGear)")
